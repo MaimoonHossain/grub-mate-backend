6a4e9190e56172a9ea98c687b925df83f824a1e6
Maimoon Hossain <ashikasitosh740@gmail.com>
2025-02-25 01:31:27 +0600

    login api implemented

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -49,4 +49,55 @@ router.post(
   }
 );
 
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Please enter a valid email address'),
+    body('password').not().isEmpty().withMessage('Password is required'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({
+        email,
+      });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'User does not exist!' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Password does not match!' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
